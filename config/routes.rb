fffd00a9572a0a533321b4764313b3404519da0c
opstech23 <bbody8215@gmail.com>
2017-11-20 15:26:30 +0800

    Added users and sessions controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  # New Login session
+  get 'sessions/new'
+
+  # New User creation 
+  get 'users/new'
+
+  get 'log_out' => "sessions#destroy", :as => "log_out"
+  get 'log_in' => "sessions#new", :as => "log_in"
+  get 'sign_up' => "users#new", :as => "sign_up"
+  resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
